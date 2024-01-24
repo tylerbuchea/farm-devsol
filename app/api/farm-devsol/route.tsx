7c4304fac524442323a9f0d710b13586381a6866
Tyler Buchea <tyler.buchea@gmail.com>
2024-01-24 15:57:36 -0400

    add farming code

---
+++ app/api/farm-devsol/route.tsx
@@ -0,0 +1,67 @@
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { NextResponse } from "next/server";
+
+const WHALE_WALLET = "";
+const RPC_URL = "https://api.devnet.solana.com";
+
+export async function GET(req: Request) {
+  try {
+    const connection = new Connection(RPC_URL, "confirmed");
+    const keypair = Keypair.generate();
+
+    console.log("requesting airdrop");
+    const signature = await connection.requestAirdrop(
+      keypair.publicKey,
+      1 * LAMPORTS_PER_SOL
+    );
+
+    console.log("airdrop requested");
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+
+    console.log("airdrop confirmed");
+    await connection.confirmTransaction(
+      {
+        blockhash,
+        lastValidBlockHeight,
+        signature,
+      },
+      "finalized"
+    );
+    console.log("airdrop success");
+
+    const recipient = new PublicKey(WHALE_WALLET);
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: keypair.publicKey,
+        toPubkey: recipient,
+        lamports: 1 * LAMPORTS_PER_SOL - 0.1 * LAMPORTS_PER_SOL,
+      })
+    );
+
+    console.log("sending solana");
+
+    const txid = await sendAndConfirmTransaction(connection, transaction, [
+      keypair,
+    ]);
+
+    console.log("solana sent txid:", txid);
+
+    return Response.json({ success: "true", txid });
+  } catch (error: any) {
+    console.error(`${req.url}`, error?.message);
+    return NextResponse.json(
+      { error: "error bad request", message: error?.message },
+      { status: 400 }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useCallback, useState } from "react";
 
 export default function Home() {
+  const [farmAmount, setFarmAmount] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleFarming = useCallback(async () => {
+    try {
+      setLoading(true);
+      await Promise.all(
+        Array(Number(farmAmount))
+          .fill(0)
+          .map(() => {
+            return fetch(`/api/farm-devsol?foo=${Math.random()}`);
+          })
+      );
+      setLoading(false);
+    } catch (error: any) {
+      setLoading(false);
+      console.error(error);
+    }
+  }, [farmAmount]);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <input
+        value={farmAmount}
+        onChange={(e) => setFarmAmount(e.target.value)}
+        placeholder="amount to farm #"
+      />
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button onClick={handleFarming} disabled={loading}>
+        FARM
+      </button>
+      {loading && <div>LOADING...</div>}
+    </div>
   );
 }
